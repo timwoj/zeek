6f7672f24d32b5306b64c948b279fb6ba2ab0bc4
Tim Wojtulewicz <timwoj@gmail.com>
2026-05-04 12:56:40 -0700

    ubuntu 24.04

--- ci/build.sh
+++ ci/build.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 set -e
 set -x
 
-if [[ "${CIRRUS_OS}" == "darwin" ]]; then
+if [[ "${ZEEK_CI_RUNNER_OS}" == "darwin" ]]; then
     # Starting with Monterey & Xcode 13.1 we need to help it find OpenSSL
     if [ -d /usr/local/opt/openssl@1.1/lib/pkgconfig ]; then
         export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/opt/openssl@1.1/lib/pkgconfig
@@ -18,9 +18,9 @@ if [[ "${ZEEK_CI_CREATE_ARTIFACT}" != "1" ]]; then
     cd build
     make -j ${ZEEK_CI_CPUS}
 else
-    ./configure ${ZEEK_CI_CONFIGURE_FLAGS} ${ZEEK_CI_CONFIGURE_FLAGS_EXTRA} --prefix=${CIRRUS_WORKING_DIR}/install
+    ./configure ${ZEEK_CI_CONFIGURE_FLAGS} ${ZEEK_CI_CONFIGURE_FLAGS_EXTRA} --prefix=${CIRCLE_WORKING_DIRECTORY}/${CIRCLE_PROJECT_REPONAME}/install
     cd build
     make -j ${ZEEK_CI_CPUS} install
     cd ..
-    tar -czf ${CIRRUS_WORKING_DIR}/build.tgz ${CIRRUS_WORKING_DIR}/install
+    tar -czf ${CIRCLE_WORKING_DIRECTORY}/${CIRCLE_PROJECT_REPONAME}/build.tgz ${CIRCLE_WORKING_DIRECTORY}/${CIRCLE_PROJECT_REPONAME}/install
 fi
