ecaf5e6904ab7bce9ee78541a5201723d85cc228
Tim Wojtulewicz <timwoj@gmail.com>
2026-05-06 17:10:00 -0700

    fixup! Add test artifacts

--- ci/test.sh
+++ ci/test.sh
@@ -73,8 +73,11 @@ function prep_artifacts {
     [[ -d .tmp ]] && rm -rf .tmp/script-coverage && tar -czf tmp.tar.gz .tmp
     junit2html btest-results.xml btest-results.html
 
-    mkdir -p ${CIRCLE_WORKING_DIRECTORY}/btest-results/$1
-    cp btest-results.xml ${CIRCLE_WORKING_DIRECTORY}/btest-results/$1/results.xml
+    if [[ -n "${CIRCLECI}" ]]; then
+        # Copy these into a location where Circle can get to them easily for the tests view
+        mkdir -p ${CIRCLE_WORKING_DIRECTORY}/btest-results/$1
+        cp btest-results.xml ${CIRCLE_WORKING_DIRECTORY}/btest-results/$1/results.xml
+    fi
 }
 
 function run_btests {
